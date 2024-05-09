ece8889e118681751c27b7daf3884d18b0f0863c
Kakyire <kakyireinc@gmail.com>
2024-05-09 02:30:57 +0000

    Initial Commit

---
+++ Push Notification Example/Push_Notification_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Push_Notification_ExampleApp.swift
+//  Push Notification Example
+//
+//  Created by Daniel Frimpong on 09/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Push_Notification_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
